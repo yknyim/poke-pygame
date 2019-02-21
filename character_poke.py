ffa778ca26ca6ee3d983e24a8a5faa9a1949beba
Ryan Yim <yknyim@gmail.com>
2019-02-21 10:39:08 -0500

    adds pikachu Character

--- character_poke.py
+++ character_poke.py
@@ -1,1 +1,37 @@
 import pygame
+
+def main():
+    width = 500
+    height = 500
+    blue_color = (97, 159, 182)
+
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('My Game')
+    clock = pygame.time.Clock()
+
+    # Game initialization
+    pikachu_image = pygame.image.load('../poke-pygame/images/pikachu.png').convert_alpha()
+
+    stop_game = False
+    while not stop_game:
+        for event in pygame.event.get():
+            # Event handling
+            if event.type == pygame.QUIT:
+                stop_game = True
+
+
+        # Game logic
+
+        # Draw background
+        screen.fill(blue_color)
+
+        # Game display
+        screen.blit(pikachu_image, (240, 260))
+        pygame.display.update()
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
